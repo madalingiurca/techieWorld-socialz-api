2dbe06ea9582e67620c3cb6bfe4d45e1753ff9c3
madalingiurca <mgiurca.mail@gmail.com>
2022-04-10 18:01:18 +0300

    add custom field name for one item response DTO

--- src/main/java/tech/madalingiurca/techieworldsocialz/web/model/response/GenericOneItemResponse.java
+++ src/main/java/tech/madalingiurca/techieworldsocialz/web/model/response/GenericOneItemResponse.java
@@ -1,8 +1,12 @@
 package tech.madalingiurca.techieworldsocialz.web.model.response;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
+
+import java.util.Collections;
+import java.util.Map;
 
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -11,10 +15,16 @@ public class GenericOneItemResponse<V> {
     @JsonIgnore
     private final String fieldName;
 
+    @JsonIgnore
     private final V field;
 
     public GenericOneItemResponse(String fieldName, V value) {
         this.fieldName = fieldName;
         this.field = value;
+    }
+
+    @JsonAnyGetter
+    public Map<String, V> setFieldName() {
+        return Collections.singletonMap(fieldName, field);
     }
 }
