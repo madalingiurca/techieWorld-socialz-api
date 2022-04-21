a4d0402fd2783b9ba5bb2a4ea27ecea367ebf2b9
madalingiurca <mgiurca.mail@gmail.com>
2022-04-21 18:57:57 +0300

    add initial data for local env

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -6,3 +6,23 @@ INSERT INTO users (id, username, alias, password, email)
 VALUES ('2', 'user2', 'joe doe2', '$2a$10$TGhynuWFfZZQ5TCkRcyH4uHEHU7DLrjJs/xZVMmtjAOlQSrtb/mXW', 'hello@lal.com');
 INSERT INTO users (id, username, alias, password, email)
 VALUES ('3', 'user3', 'joe doe3', '$2a$10$TGhynuWFfZZQ5TCkRcyH4uHEHU7DLrjJs/xZVMmtjAOlQSrtb/mXW', 'hello2@lal.com');
+
+
+Insert into posts (ID, AUTHOR, CONTENT, CREATION_DATE)
+VALUES ('1', 'Johnutzu',
+        'Morbi sodales elit nec neque tempor, in pellentesque quam porta. Ut imperdiet, ligula ac sollicitudin luctus, ex lacus tempor mauris, eu gravida purus neque vel neque. Vivamus condimentum condimentum purus, eget feugiat arcu mollis a. Suspendisse dolor erat, ultricies et urna sed, ultricies dictum turpis.',
+        {ts '2021-09-17 18:47:52.69'});
+
+Insert into posts (ID, AUTHOR, CONTENT, CREATION_DATE)
+VALUES ('2', 'Joni',
+        'Ut rhoncus elementum sapien, eget eleifend lorem. Duis ac ante rutrum, eleifend lacus vel, iaculis purus. Pellentesque a molestie arcu, sed lacinia lorem. Fusce mauris mauris, eleifend sit amet condimentum a, condimentum id enim. Aliquam facilisis erat non pharetra viverra.',
+        {ts '2022-04-01 13:17:52.69'});
+
+Insert into posts (ID, AUTHOR, CONTENT, CREATION_DATE)
+VALUES ('3', 'ArianaGrande', 'Here I am again on my own, no ctime to spend on all of these',
+        {ts '2022-04-04 11:47:51.69'});
+
+Insert into posts (ID, AUTHOR, CONTENT, CREATION_DATE)
+VALUES ('4', 'MlonEusk',
+        'Phasellus porttitor mollis arcu et ultricies. Nullam vitae ligula id purus ultricies mattis. Nunc laoreet rhoncus metus, ac ultricies neque tincidunt vel. Duis in suscipit lacus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec a fermentum nunc, ac sodales lacus. Aenean enim nisl, luctus sollicitudin sapien sit amet, tristique placerat lacus. In hac habitasse platea dictumst. Integer interdum tortor leo, quis venenatis metus facilisis in. Vivamus suscipit sapien elementum ligula scelerisque, vitae accumsan odio accumsan.',
+        {ts '2022-04-10 18:23:50.69'});
